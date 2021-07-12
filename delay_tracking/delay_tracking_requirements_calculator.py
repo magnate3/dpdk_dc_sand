7255a34c1190fec342b0899e7c9b2c3015ad998a
tyronevb <tvanballa@ska.ac.za>
2021-07-12 17:03:36 +0200

    Refactor delay tracking requirements calculator script
    The script for calculating the delay tracking requirements has been
    refactored:
    - Callable from the command line with flags that can be set to
      accommodate different formats and structures of the csv file
      containing the antenna coordinates
    - all the calculations have been encapsulated in functions
    - code structure was refactored to be more in line with standard Python
      coding style
    - Double checked all calculations and correct usage of latitude and
      longitude coordinates
    
     Contributes to: NGC-230

--- delay_tracking/delay_tracking_requirements_calculator.py
+++ delay_tracking/delay_tracking_requirements_calculator.py
@@ -1,5 +1,5 @@
 """
-Script to calculate MeerKAT Extension phase tracking requirements.
+Script to calculate MeerKAT Extension delay tracking requirements.
 
 This script loads the MeerKAT Extension antenna coordinates, determines the maximum baseline length from this and then
 calculates the MeerKAT Extension delay tracking requirements introduced by this distance.
@@ -8,102 +8,100 @@ The requirements that this script is meant to determine include:
 1. The maximum baseline length that needs to be accounted for.
 2. The maximum delay that the system needs to compensate for.
 3. The maximum and minumum rate of change of delay.
-
-TODO: Split this script into two scripts, one to determine the maximum baseline and one to determine the delay tracking
-requirements from this maximum baseline.
 """
 
 __author__ = "Gareth Callanan"
+__maintainer__ = "Tyrone van Balla"
 
-import geopy.distance
+import argparse
+import csv
+import itertools
+from typing import Tuple
+
 import numpy
-import pandas as pd
 import scipy
 import scipy.constants
+from geopy import distance
 
 
-def calculate_distances_km(ant1_coords, ant2_coords):
-    """Calculate the distance between two GPS coordinates.
-
-    Calculates the distance between two coordinates on the globe. It also projects the distance onto the equator and
-    returns this. I thought this projection was important for delay tracking, but the baseline distance is more
-    applicable. However the calculation has been left here due to laziness.
-
-    ant1_coords and ant2_coords are expected to each be a tuple containg latitude and longitude coordinates in the
-    decimal degrees format. Example: (14.333,-28.444). The latitude is the first entry and longitude is the second
-    entry.
+def get_coordinates_from_csv(
+    path_to_csv: str, delimiter: str = ",", col_labels: bool = True, latitude_first: bool = False
+) -> dict:
     """
-    horiz_dist_coords = ant2_coords[1] - ant1_coords[1]
-    vertical_dist_coords = ant2_coords[0] - ant1_coords[0]
-    absolute_distance_km = geopy.distance.GeodesicDistance(ant1_coords, ant2_coords).kilometers
-    angle = numpy.arctan2(vertical_dist_coords, horiz_dist_coords)
-    horizontal_distance_km = abs(absolute_distance_km * numpy.cos(angle))
-    return absolute_distance_km, horizontal_distance_km
+    Read antenna coordinates from csv file into a dictionary.
 
+    Assumes coordinates are formatted as decimal degrees.
+    Expected format of each entry in the csv: ANT_NAME, Latitude, Longitude OR ANT_NAME, Longitude, Latitude
+    Column labels/headings in the CSV file are optional.
 
-print("Starting")
-
-print()
-print("Loading Antennas Coords from a CSV file.")
-# The antenna data is expected to be in csv format with three columns. The first row must have column headings:
-# "Name,Longitude,Latitude". Each row contains an entry for a different antenna. The latitude and longitude coordinates
-# must be formatted as decimal degrees. An example of a row entry is "ANT01,14.333,-28.444."
-data = pd.read_csv("meerkat_extension_preliminary_antenna_locations.csv")
-latNames = numpy.array(data["Name"])
-latArr = numpy.array(data["Latitude"])
-longArr = numpy.array(data["Longitude"])
-
-antCoords = list(zip(latArr, longArr))
-numAnts = len(antCoords)
-
-print()
-print("Calculating Baseline Lengths:")
-
-distances = []
-for i in range(numAnts):
-    for j in range(i + 1, numAnts):
-        absolute_distance_km, horizontal_distance_km = calculate_distances_km(antCoords[i], antCoords[j])
-        distances.append((latNames[i], latNames[j], absolute_distance_km, horizontal_distance_km))
-
-absolute_distance_km, horizontal_distance_km = calculate_distances_km(antCoords[0], antCoords[1])
-
-distances.sort(key=lambda x: x[2])  # Not technically needed
-max_distance_absolute_tuple = max(distances, key=lambda x: x[2])
-max_distance_horizontal_tuple = max(distances, key=lambda x: x[3])
-
-print(f"\tMax Distance Tuple: {max_distance_absolute_tuple}")
-print(f"\tMax Horizontal Distance Tuple: {max_distance_horizontal_tuple}")
-
-# The maximum baseline length determines the maximum coarse delay - this corresponds to an object just on the horizon
-# where the wavefront needs to travel almost directly along the baseline.
-max_absolute_distance_m = max_distance_absolute_tuple[2] * 1000
-max_coarse_delay_s = max_absolute_distance_m / scipy.constants.c
-
-print()
-print("Calculating Maximum Compensation Delay Requirements:")
-
-print(f"\tMax Comp Delay with distance of {max_absolute_distance_m:.2f} m: {max_coarse_delay_s*1000*1000:.2f} us")
-
-# Technically what was done for MeerKAT is to take "max_coarse_delay_s" and double it - this is done to account for a
-# virtual reference antenna. It is not absolutly necessary to double it, this is just the way we have implemented it.
-# You could just as easily have added a constant of some us to get a delay of "max_coarse_delay_s + x us"
-#
-# We then need to account for the different propagation times in the PPS signal. This signal originates in the central
-# MeerKAT data centre (The KAPB). The cable lengths this signal is sent on are not equal. The different in time that needs
-# to be accounted for is the different in propagation time between the shortest and longest cable. A more than worst
-# case estimate for this length is the length of the longest baseline. As such we now have
-# "2 * max_coarse_delay_s + ~max_coarse_delay_s ~= 3 * max_coarse_delay_s"
-print(
-    f"\tMultiply by three to account for PPS propagation and virtual reference antenna: {max_coarse_delay_s*1000*1000*3:.2f} us"
-)
-
-print()
-print(f"Calculating maximum and minimum delay rates for a baseline length of {max_absolute_distance_m} m:")
-
-
-def calculate_delay_from_source_elevation(baseline_length_m, source_elevation_degrees):
+    :param path_to_csv: path to csv file containing dish positions specified by geographic coordinates
+    :param delimiter: specify what delimiter is used in the csv file, default=,
+    :param col_labels: set to False if csv file does not contain column labels
+    :param latitude_first: set to True if latitude coodinate is specified first in the csv file
+    :return: dict() Each item in the returned dict has the structure key=ant_name, value=(latitude, longitude)
     """
-    Determine the delay between two antennas in seconds based on a source's elevation.
+    tmp = list()
+    with open(path_to_csv, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=delimiter)
+        for row in reader:
+            tmp.append(row)
+
+    if col_labels:
+        tmp.pop(0)  # remove column labels if present
+
+    if latitude_first:
+        ant_coords = {ant[0]: (ant[1], ant[2]) for ant in tmp}
+    else:
+        ant_coords = {ant[0]: (ant[2], ant[1]) for ant in tmp}
+
+    return ant_coords
+
+
+def calculate_baselines(antenna_pos_coords: dict) -> dict:
+    """
+    Calculate baselines for a given set of antennas described by their geographic coordinates.
+
+    :param antenna_pos_coords: dictionary containing antenna names as keys and their geographic coords,
+    in a tuple, as values
+    :return: dict() each item in the returned dict has the structure key=(ant01, ant02), value=baseline_in_km
+    """
+    # each item in baselines is of form {key=(ant01, ant02), value=baseline_in_km}
+    baselines = dict()
+
+    # iterate through all pairs of antennas
+    for ant_pair in itertools.product(antenna_pos_coords, repeat=2):
+        ant_a = (
+            antenna_pos_coords[ant_pair[0]][0],
+            antenna_pos_coords[ant_pair[0]][1],
+        )  # get lat and long of first ant
+        ant_b = (
+            antenna_pos_coords[ant_pair[1]][0],
+            antenna_pos_coords[ant_pair[1]][1],
+        )  # get lat and long of second ant
+
+        # calculate baseline between antenna pair
+        baselines[ant_pair] = distance.GeodesicDistance(ant_a, ant_b).kilometers
+
+    return baselines
+
+
+def find_longest_baseline(baselines: dict) -> Tuple[str, float]:
+    """
+    Retrieve the longest baseline from a set of baselines calculated for an arrangment of antennas.
+
+    :param baselines: dictionary containing antennas pairs as keys and baselines as values
+    :return:  vanteanna_pair_with_longest_baselines, baseline_in_km
+    """
+    # get the maximum value and return the corresponding key and value
+    longest_baseline_ants = max(baselines, key=lambda x: x[1])
+    longest_baseline = baselines[longest_baseline_ants]
+
+    return (longest_baseline_ants, longest_baseline)
+
+
+def calculate_delay_from_source_elevation(baseline: float, source_elevation_degrees: float) -> float:
+    """
+    Calculate the delay between two antennas in seconds based on a source's elevation.
 
     For the purposes of this calculation a right angle triangle is constructed. The baseline
     vector is the hypotenuse of this triangle. The wavefront from the source is the second side.
@@ -112,39 +110,140 @@ def calculate_delay_from_source_elevation(baseline_length_m, source_elevation_de
     antenna from the wavefront. This third side is the length of delay between the two antennas.
     The angle between the third side and the baseline vector is equal to this elevation angle.
 
-    A diagram is probably more useful to understand this. One should be available in the
-    git repo this script is commited in.
+    A diagram is included in the repo in which this script is located.
+    :param baseline: distance between the two antennas considered, for specifications, use the maximum baseline, in m
+    :param source_elevation_degrees: elevation to the point source, specificed in degrees
     """
     if source_elevation_degrees > 90 or source_elevation_degrees < 0:
         raise TypeError("source_elevation_degrees needs to be within: 0<=x<=90")
 
-    # Calculate the delay distance using properties of the triangle constructed above
-    delay_length_m = baseline_length_m * numpy.cos(source_elevation_degrees / 180 * scipy.constants.pi)
+    # Calculate the delay distance using properties of the triangle described in the docstring above
+    delay_length_m = baseline * numpy.cos(source_elevation_degrees / 180 * scipy.constants.pi)
     delay_length_s = delay_length_m / scipy.constants.c
 
     return delay_length_s
 
 
-# The model here assumes the earth remains stationary, and the source moves across the sky changing the wavefront
-# angle at a uniform rate.
-# Calculation is for an elevation change by 90 degrees over six hours.
-elevationDeltaPerSecond = 90 / (3600 * 6)
+def calculate_delay_rate_of_change(
+    baseline: float, elevation: float, elevation_change_per_second: float, max_or_min: str
+) -> float:
+    """
+    Calculate delay rate of change required for a source point at a given elevation.
 
-# Maximum rate of change of delay occurs at 90 degree elevation
-elevationMax = 90
-delay1_s = calculate_delay_from_source_elevation(max_absolute_distance_m, elevationMax)
-delay2_s = calculate_delay_from_source_elevation(max_absolute_distance_m, elevationMax - elevationDeltaPerSecond)
-deltaDelayMax_nanosecondsPerSecond = (delay2_s - delay1_s) * 1000 * 1000 * 1000
+    :param baseline: distance between the two antennas considered, for specifications, use the maximum baseline, in m
+    :param elevation: elevation to the point source, specificed in degrees
+    :param elevation_change_per_second: change in elevation, degrees per second
+    :param max_or_min: specify whether the maximum or minimum rate of change is being calculated
+    """
+    delay1_s = calculate_delay_from_source_elevation(baseline, elevation)
+    if max_or_min == "max":
+        delay2_s = calculate_delay_from_source_elevation(baseline, elevation - elevation_change_per_second)
+    else:
+        delay2_s = calculate_delay_from_source_elevation(baseline, elevation + elevation_change_per_second)
 
-# Minimum rate of change of delay occurs at 0 degree elevation. However we only consider objects 15 degrees above the
-# horizon.
-elevationMin = 15
-delay1_s = calculate_delay_from_source_elevation(max_absolute_distance_m, elevationMin)
-delay2_s = calculate_delay_from_source_elevation(max_absolute_distance_m, elevationMin + elevationDeltaPerSecond)
-deltaDelayMin_nanosecondsPerSecond = (delay1_s - delay2_s) * 1000 * 1000 * 1000
+    delay_rate_of_change = abs((delay2_s - delay1_s) * 1000 * 1000 * 1000)
 
-print(f"\tMaximum Delay occuring at elevation of {elevationMax} degrees: {deltaDelayMax_nanosecondsPerSecond} ns/s")
-print(f"\tMinimum Delay occuring at elevation of {elevationMin} degrees: {deltaDelayMin_nanosecondsPerSecond} ns/s")
+    return delay_rate_of_change
 
 
-print("Done")
+def calculate_delay_tracking_requirements(longest_baseline: float) -> dict:
+    """
+    Calculate the delay tracking requirements for a given maximum baseline.
+
+    :param longest_baseline: maximum baseline to be considered in the derivation of requirements, in km
+    :return: dict() containing all delay tracking specifications
+    """
+    # calculate maximum delay compensation required
+
+    # The maximum baseline length determines the maximum coarse delay - this corresponds to an object just on the
+    # horizon where the wavefront needs to travel almost directly along the baseline.
+    longest_baseline_m = longest_baseline * 1000  # convert to metres
+    max_coarse_delay_s = longest_baseline_m / scipy.constants.c
+
+    # Technically what was done for MeerKAT is to take "max_coarse_delay_s" and double it - this is done to account for
+    # a virtual reference antenna. It is not absolutly necessary to double it, this is just the way we have
+    # implemented it. You could just as easily have added a constant of some us to get a delay
+    # of "max_coarse_delay_s + x us"
+
+    # We then need to account for the different propagation times in the PPS signal. This signal originates in the
+    # central MeerKAT data centre (The KAPB). The cable lengths this signal is sent on are not equal. The difference in
+    # time that needs to be accounted for is the difference in propagation time between the shortest and longest cable.
+    # A more than worst case estimate for this length is the length of the longest baseline. As such, the maximum delay
+    # compensation required becomes "2 * max_coarse_delay_s + ~max_coarse_delay_s ~= 3 * max_coarse_delay_s"
+
+    max_coarse_delay_s *= 3
+
+    # calculate range of rate of change of delay
+
+    # The model here assumes the earth remains stationary, and the source moves across the sky changing the wavefront
+    # angle at a uniform rate.
+    # Calculation is for an elevation change by 90 degrees over six hours.
+    elevation_change_per_second = 90 / (3600 * 6)
+
+    # maximum and minimum elevation:
+    # maximum rate of change of delay occurs at 90 degrees elevation
+    # minumum rate of change of delay occurs at 15 degrees elevation
+    elevation = {"min": 15, "max": 90}
+
+    # rate of change is specific in picoseconds/second
+    range_of_rate_of_change = {
+        k: calculate_delay_rate_of_change(
+            baseline=longest_baseline_m,
+            elevation=v,
+            elevation_change_per_second=elevation_change_per_second,
+            max_or_min=k,
+        )
+        for (k, v) in elevation.items()
+    }
+
+    return {
+        "longest_baseline_km": longest_baseline_m / 1000,
+        "max_coarse_delay_us": max_coarse_delay_s * 1000 * 1000,
+        "range_of_rate_of_change_of_delay_ns_s": range_of_rate_of_change,
+    }
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "ant_pos", type=str, help="csv file containing " "antenna positions defined by geographic coordinates"
+    )
+    parser.add_argument("-d", dest="delimiter", type=str, help="delimter in csv " "file", default=",")
+    parser.add_argument(
+        "-l",
+        dest="lat_first",
+        action="store_true",
+        help="specify that latitude coords appear " "first in the given csv file",
+    )
+    parser.add_argument(
+        "-n", dest="no_col_labels", action="store_true", help="specify that no column " "labels are present"
+    )
+
+    args = parser.parse_args()
+
+    ant_coords = get_coordinates_from_csv(
+        path_to_csv=args.ant_pos,
+        delimiter=args.delimiter,
+        col_labels=not args.no_col_labels,
+        latitude_first=args.lat_first,
+    )
+
+    baselines = calculate_baselines(antenna_pos_coords=ant_coords)
+
+    longest_baseline_ants, longest_baseline_km = find_longest_baseline(baselines=baselines)
+
+    delay_tracking_reqs = calculate_delay_tracking_requirements(longest_baseline=longest_baseline_km)
+
+    print("================================================================")
+    print("Correlator Delay Tracking Requirements")
+    print("================================================================")
+    print(f"Longest Baseline: {longest_baseline_km:.2f} km")
+    print(f"Longest Baseline Antenna Pair: {longest_baseline_ants}")
+    print(f"Maximum Delay Compensation: {delay_tracking_reqs['max_coarse_delay_us']:.2f} us")
+    print(
+        "Range of rate of change of delay: ",
+        f"<={delay_tracking_reqs['range_of_rate_of_change_of_delay_ns_s']['min']:.2f}",
+        f"ns/s to >= {delay_tracking_reqs['range_of_rate_of_change_of_delay_ns_s']['max']:.2f} ns/s",
+    )
+    print("================================================================")
