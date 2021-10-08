f8990502d213e09841c08fa5d96fe0121aed4a12
Amish <apatel@sarao.ac.za>
2021-10-08 10:39:15 +0000

    Add setup and init files for installation
    This is yet to be tested, but adding infrastructure for development.
    
    Contributes to: NGC-394.

---
+++ dsim/setup.py
@@ -0,0 +1,13 @@
+"""
+Installer for dsim development module.
+
+"""
+from setuptools import setup
+
+__VERSION__ = "0.0.1"
+
+setup(
+    name="dsim",
+    version=__VERSION__,
+    packages=["dsim"],
+)
