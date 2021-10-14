831d499060c553ce8c8f904550c345811e0b6856
Bruce Merry <bmerry@sarao.ac.za>
2021-10-14 12:39:29 +0200

    Rewrite the test logging functionality

--- pdf_logger/test/conftest.py
+++ pdf_logger/test/conftest.py
@@ -1,110 +1,46 @@
-"""Hooks and fixtures for logging Pytest's output to a PDF.
+"""Fixtures for capturing Hooks and fixtures for logging Pytest's output to a PDF.
 
 Commented out functions are there to remind me that the hooks exist, in case I
 should need them in future tinkerings.
 """
-import logging
-import os
-import subprocess
+
+from typing import Optional
 
 import pytest
 
-from pdf_logger.json_handler import JsonHandler
 
-# Take charge of the logging.
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+class Reporter:
+    """Provides mechanisms to log steps taken in a test."""
 
-output_dir = "results"
-output_filename = "report"
+    def __init__(self, data: list) -> None:
+        self._data = data
+        self._cur_step: Optional[list] = None
 
-# Make sure we're using the Json Handler created for the purpose.
-my_handler = JsonHandler(output_filename=output_filename, destination_dir=output_dir)
-logger.addHandler(my_handler)
+    def step(self, message: str) -> None:
+        """Report the start of a high-level step."""
+        self._cur_step = []
+        self._data.append({"$msg_type": "step", "message": message, "details": self._cur_step})
 
-# Because the Json handler keeps its own internal state, to help with debugging
-# that, we add a normal file handler as well, which will just log everything. It
-# makes picking through the JSON output easier.
-file_handler = logging.FileHandler(os.path.join(output_dir, "raw_output.log"))
-file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(funcName)s - %(message)s"))
-logger.addHandler(file_handler)
+    def detail(self, message: str) -> None:
+        """Report a low-level detail, associated with the previous call to :meth:`step`."""
+        if self._cur_step is None:
+            raise ValueError("Cannot have detail without a current step")
+        self._cur_step.append({"$msg_type": "detail", "message": message})
 
 
 @pytest.fixture
-def skarab_dsim_fixture():
+def pdf_report(request) -> Reporter:
+    """Fixture for logging steps in a test."""
+    data = [{"$msg_type": "test_info", "blurb": request.node.function.__doc__}]
+    request.node.user_properties.append(("pdf_report_data", data))
+    return Reporter(data)
+
+
+@pytest.fixture
+def skarab_dsim_fixture(pdf_report):
     """SKARAB DSim."""
-    logging.info("Setting up SKARAP Dsim")
-    logging.debug("skarab020406, firmware 0.1")
+    pdf_report.step("Setting up SKARAP Dsim")
+    pdf_report.detail("skarab020406, firmware 0.1")
     yield "SKARAB DSIM"
-    logging.info("Tearing down SKARAB dsim.")
-    logging.debug("DSim teardown finished.")
-
-
-def pytest_runtest_logstart(nodeid, location):
-    """Indicate that we are starting the test."""
-    logging.warning(f"START {nodeid} {location}")
-
-
-def pytest_runtest_logfinish(nodeid, location):
-    """Emit a warning when a test is finished."""
-    logging.warning(f"STOP {nodeid} {location}")
-
-
-# def pytest_report_collectionfinish(config, startdir, items: Sequence[pytest.Item]):
-#     """We can get a list of tests that are actually going to run."""
-#     logging.error("Actually running the hook.")
-#     for test in items:
-#         logging.error(test)
-#     logging.error("Finished with the for loop.")
-
-
-# def pytest_assertion_pass(item, lineno, orig, expl):
-#     logging.debug(f"Assertion passed: {expl}")
-
-# def pytest_assertrepr_compare(config, op, left, right):
-#     logging.error(f"Assert failed: {left} {op} {right}")
-
-
-def pytest_fixture_setup(fixturedef, request):
-    """Log the fixture's docstring."""
-    extra = {"fixture_docstring": fixturedef.func.__doc__}
-    logging.debug(f"FIXTURE_SETUP {extra}", extra=extra)
-
-
-# def pytest_fixture_post_finalizer(fixturedef, request):
-#     logging.debug("In the post-fixture-finalizer hook.")
-
-
-# def pytest_runtest_setup(item: pytest.Item):
-#     logging.debug(f"TEST SETUP {item.function.__name__}")
-
-
-def pytest_runtest_call(item: pytest.Item):
-    """Log the docstring of the function before you actually run it."""
-    extra = {"test_name": item.function.__name__, "test_docstring": item.function.__doc__}
-    logging.debug(f"BLURB {extra}", extra=extra)
-
-
-# def pytest_runtest_teardown(item: pytest.Item, nextitem: pytest.Item):
-#     logging.debug(f"TEST TEARDOWN {item.function.__name__}")
-
-
-def pytest_runtest_logreport(report):
-    """Log the outcome of the test.
-
-    We only do the actual test itself, not setup or teardown.
-    """
-    if report.when == "call":  # As opposed to setup and teardown. This is where to get the actual outcome of the test.
-        extra = {"outcome": report.outcome, "longrepr": str(report.longrepr)}
-        logging.debug(f"OUTCOME {extra}", extra=extra)
-
-
-def pytest_sessionfinish():
-    """Shut down logging and generate output files."""
-    logging.shutdown()  # If we don't do this, the output files aren't closed yet.
-    pdf_path = os.path.join(output_dir, output_filename + ".tex")
-
-    # Generate the actual PDF.
-    subprocess.run(["latexmk", "-cd", pdf_path, "-pdf"])
-    # Cleans up the unnecessary stuff.
-    subprocess.run(["latexmk", "-cd", pdf_path, "-c"])
+    pdf_report.step("Tearing down SKARAB dsim.")
+    pdf_report.detail("DSim teardown finished.")
--- pdf_logger/test/test_json_handler.py
+++ pdf_logger/test/test_json_handler.py
@@ -1,57 +1,59 @@
 # noqa: D100
-from pdf_logger.convenience import check, detail, step
 
 
-def test_channelisation(skarab_dsim_fixture):
+def test_channelisation(pdf_report, skarab_dsim_fixture):
     """Channelisation Test.
 
     Fulfils requirement ABC.
     """
-    step("Select a random channel.")
-    detail("Random channel selected: 42")
+    pdf_report.step("Select a random channel.")
+    pdf_report.detail("Random channel selected: 42")
 
-    step("Sweep frequencies across that channel and capture heaps.")
-    detail("Setting Dsim frequency to 1")
-    detail("Heap captured.")
-    detail("Setting Dsim frequency to 2")
-    detail("Heap captured.")
-    detail("Setting Dsim frequency to 3")
-    detail("Heap captured.")
+    pdf_report.step("Sweep frequencies across that channel and capture heaps.")
+    pdf_report.detail("Setting Dsim frequency to 1")
+    pdf_report.detail("Heap captured.")
+    pdf_report.detail("Setting Dsim frequency to 2")
+    pdf_report.detail("Heap captured.")
+    pdf_report.detail("Setting Dsim frequency to 3")
+    pdf_report.detail("Heap captured.")
 
-    step("Check peak is in centre of the channel.")
-    check(1 in [1, 2, 3], "Peak not in centre of channel!")
-    detail("Peak located in centre of channel.")
+    pdf_report.step("Check peak is in centre of the channel.")
+    assert 1 in [1, 2, 3], "Peak not in centre of channel!"
+    pdf_report.detail("Peak located in centre of channel.")
 
-    step("Check response far away is below -62 dB")
-    check(0 < 1, "Rejection not good enough!")
-    detail("Out of channel rejection is within spec.")
+    pdf_report.step("Check response far away is below -62 dB")
+    assert 0 < 1, "Rejection not good enough!"
+    pdf_report.detail("Out of channel rejection is within spec.")
 
 
-def test_delay_tracking(skarab_dsim_fixture):
+def test_delay_tracking(pdf_report, skarab_dsim_fixture):
     """Delay tracking test.
 
     Fulfils requirement FGD.
     """
-    step("Set delay model on Antenna 1")
-    detail("Setting delay model on antenna 1")
-    check(1 <= 1.2, "Delay model not correctly configured.")
-    detail("Antenna 1 delay model confirmed as 345.")
+    pdf_report.step("Set delay model on Antenna 1")
+    pdf_report.detail("Setting delay model on antenna 1")
+    assert 1 <= 1.2, "Delay model not correctly configured."
+    pdf_report.detail("Antenna 1 delay model confirmed as 345.")
 
-    step("Capture 3 heaps, compare the phase, check for within spec.")
-    detail("Captured heap 1")
-    detail("Captured heap 2")
-    detail("Captured heap 3")
-    check(2 < 1, "Phase not within spec!")
+    pdf_report.step("Capture 3 heaps, compare the phase, check for within spec.")
+    pdf_report.detail("Captured heap 1")
+    pdf_report.detail("Captured heap 2")
+    pdf_report.detail("Captured heap 3")
+    a = 2
+    b = 1
+    assert a < b, "Phase not within spec!"
 
 
-def test_baselines(skarab_dsim_fixture):
+def test_baselines(pdf_report, skarab_dsim_fixture):
     """Baseline test.
 
     Blurb.
 
     Fulfils another requirement.
     """
-    step("Set something.")
-    detail("Checking that something is set.")
-    detail("Confirmed, something is set properly.")
-    check(7 < 8, "Seven not less than 8.")
+    pdf_report.step("Set something.")
+    pdf_report.detail("Checking that something is set.")
+    pdf_report.detail("Confirmed, something is set properly.")
+    # Test a bare assert without a message
+    assert 9 < 8
