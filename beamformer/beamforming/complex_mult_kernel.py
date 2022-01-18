Added fix for failing beamform_mult unit test. Turned out the thread execution in the GPU for the beamform coefficient computation was not being limited to valid threads and combinations of array size and channels would result in extra threads (ithread_idx) running due to rounding. This would access and corrupt memory entries for the input data.

beamformer/beamforming/complex_mult_kernel.py
@@ -81,8 +81,8 @@ def run_complex_mult(data_matrix: np.ndarray, coeff_matrix: np.ndarray, out: np.
 
         isample_per_block_index = iremindex // ants
 
-        if ithreadindex_x == 0:
-            print('ants is:', ants)
+        # if ithreadindex_x == 0:
+        #     print('ants is:', ants)
 
         for col in range(n_beams * 2):
             tmp = float32(0)
@@ -91,26 +91,26 @@ def run_complex_mult(data_matrix: np.ndarray, coeff_matrix: np.ndarray, out: np.
                 data = data_matrix[ibatchindex][ipolindex][ichanindex][iblockindex][isample_per_block_index][ant]
                 tmp += data * coeff
 
-                if ithreadindex_x <= 0:
-                    print('thread is:',ithreadindex_x,
-                    'ibatchindex:', ibatchindex,
-                    'ipolindex:', ipolindex,
-                    'ichanindex:', ichanindex,
-                    'iblockindex:', iblockindex,
-                    'isample_per_block_index:', isample_per_block_index,
-                    'ant:', ant,
-                    'col:', col, 
-                    'tmp:', tmp, 
-                    'coeff:',coeff, 
-                    'data:', data)
+                # if ithreadindex_x <= 0:
+                #     print('thread is:',ithreadindex_x,
+                #     'ibatchindex:', ibatchindex,
+                #     'ipolindex:', ipolindex,
+                #     'ichanindex:', ichanindex,
+                #     'iblockindex:', iblockindex,
+                #     'isample_per_block_index:', isample_per_block_index,
+                #     'ant:', ant,
+                #     'col:', col,
+                #     'tmp:', tmp,
+                #     'coeff:',coeff,
+                #     'data:', data)
 
-                if ibatchindex == 0:
-                    if ipolindex == 0:
-                        if ichanindex == 0:
-                            if iblockindex == 0:
-                                if isample_per_block_index == 0:
-                                    if ant == 0:
-                                        a = 1
+                # if ibatchindex == 0:
+                #     if ipolindex == 0:
+                #         if ichanindex == 0:
+                #             if iblockindex == 0:
+                #                 if isample_per_block_index == 0:
+                #                     if ant == 0:
+                #                         a = 1
                                         # print(ithreadindex_x)
                                         # print('col is:', col, 'and tmp is:', tmp, 'and coeff is:',coeff, 'and data is:', data)
 

beamformer/unit_test/test_parameters.py
@@ -9,7 +9,12 @@ batches = [3]
 # not final. Additionally values 5,23,61 and 19 are just there to test that various non-power-of-two array sizes will
 # run.
 # array_size = [4, 7, 8, 16, 32, 64, 80, 90, 128, 150, 256]
-array_size = [7, 14, 21, 28]
+# array_size = [7, 14, 21, 28]
+array_size = []
+array_min = 4
+for i in range(256):
+    array_size.append(array_min+i)
+# array_size = [80]
 # array_size = [4, 8, 16, 32, 64, 84, 130, 192, 256, 5, 23, 61, 19]
 
 """n_samples_per_channel: List of number of samples per channel."""
@@ -20,11 +25,11 @@ n_samples_per_channel = [256]
 
 """n_channels: List of number of total channels per array."""
 # Number of FFT channels out of the F-Engine
-n_channels = [1024]
+n_channels = [8192, 16384, 32768]
 
 
 """n_beams: List of number of beams."""
-n_beams = [2, 4]
-xeng_id = [0, 1]
-samples_delay = [5, 7]
-phase = [np.pi / 2, np.pi]
+n_beams = [2]
+xeng_id = [0]
+samples_delay = [5]
+phase = [np.pi / 2]