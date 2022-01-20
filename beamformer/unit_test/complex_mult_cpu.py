c080cdd2253f0d3c923a8f24603fb8687b98951c
avanderbyl <avanderbyl@sarao.ac.za>
2022-01-20 14:36:31 +0000

    Ommitted comple_mult_cpu.py from previous commit.

--- beamformer/unit_test/complex_mult_cpu.py
+++ beamformer/unit_test/complex_mult_cpu.py
@@ -39,33 +39,35 @@ def complex_mult(
         Output array of beamformed data.
     """
     output_data = np.empty(output_data_shape, dtype=np.float32)
-    batches = np.shape(input_data)[0]
-    pols = np.shape(input_data)[1]
-    n_channel = np.shape(input_data)[2]
-    blocks = np.shape(input_data)[3]
+    n_batches = np.shape(input_data)[0]
+    n_pols = np.shape(input_data)[1]
+    n_channels_per_stream = np.shape(input_data)[2]
+    n_blocks = np.shape(input_data)[3]
     n_samples_per_block = np.shape(input_data)[4]
-    ants = np.shape(input_data)[5]
-    beams = np.shape(coeffs)[4]
+    n_ants = np.shape(input_data)[5]
+    n_beams = np.shape(coeffs)[4]
 
-    for b in range(batches):
-        for p in range(pols):
-            for c in range(n_channel):
-                for block in range(blocks):
+    for b in range(n_batches):
+        for p in range(n_pols):
+            for c in range(n_channels_per_stream):
+                for block in range(n_blocks):
                     for s in range(n_samples_per_block):
-                        for beam in range(beams // 2):
+                        for beam in range(n_beams // 2):
                             data_cmplx = []
                             coeff_cmplx = []
-                            for a in range(ants):
+                            for a in range(n_ants):
                                 # Create complex valued pair for coefficients
                                 dtmp_cmplx = complex(
-                                    input_data[b, p, c, block, s, a, 0], input_data[b, p, c, block, s, a, 1]
+                                    input_data[b, p, c, block, s, a, 0],
+                                    input_data[b, p, c, block, s, a, 1],
                                 )
                                 # Append complex valued pair to form an array of <real,imag> values
                                 data_cmplx.append(dtmp_cmplx)
 
                                 # Create complex valued pair for coefficients
                                 ctmp_cmplx = complex(
-                                    coeffs[b, p, c, a * 2, beam * 2], coeffs[b, p, c, a * 2, beam * 2 + 1]
+                                    coeffs[b, p, c, a * 2, beam * 2],
+                                    coeffs[b, p, c, a * 2, beam * 2 + 1],
                                 )
 
                                 # Append complex valued pair to form an array of <real,imag> values
@@ -75,6 +77,10 @@ def complex_mult(
                             cmplx_prod = np.dot(data_cmplx, coeff_cmplx)
 
                             # Assign real and imaginary results to repective positions
-                            output_data[b, p, c, block, s, beam * 2] = np.real(cmplx_prod)
-                            output_data[b, p, c, block, s, beam * 2 + 1] = np.imag(cmplx_prod)
+                            output_data[b, p, c, block, s, beam * 2] = np.real(
+                                cmplx_prod
+                            )
+                            output_data[b, p, c, block, s, beam * 2 + 1] = np.imag(
+                                cmplx_prod
+                            )
     return output_data
