8a70f032285172d1987c30da187933841b0f6da2
avanderbyl <avanderbyl@sarao.ac.za>
2022-05-13 15:20:27 +0000

    Test versons of half(fp16) and full (fp32) precision FFT on GPU. Note: Results from cuPy (half) FFT don't match expected from non-cuPy (GPU) FFT (array length 2x). To be investigated.

---
+++ fft_analysis/fft_standalone_fp16_fp32.py
@@ -0,0 +1,131 @@
+#Pinned memory based on example: https://gist.github.com/sjperkins/d9e6db1b2d6038febb72
+import pycuda.driver as cuda
+import resource
+import numpy as np
+from time import process_time
+from skcuda import cufft as cf
+import pycuda.autoinit
+from pycuda import gpuarray
+import matplotlib.pyplot as plt
+from IPython import embed
+from pycuda.compiler import SourceModule
+import pycuda.tools as tools
+
+
+# --- FP16 ---
+#source: https://docs.cupy.dev/en/stable/user_guide/fft.html
+import cupy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+
+shape = (1, 1, 8)  # input array shape
+
+idtype = odtype = edtype = 'E'  # = numpy.complex32 in the future
+
+# store the input/output arrays as fp16 arrays twice as long, as complex32 is not yet available
+a = 0.005*cp.random.random((shape[0], shape[1], 2*shape[2])).astype(cp.float16)
+
+# vec_len = np.power(2,3)
+# d_first_real = cp.ones((1,), dtype=np.float16)
+# d_second_real = cp.zeros((vec_len-1,), dtype=np.float16)
+# a = cp.concatenate([d_first_real, d_second_real])
+
+out = cp.empty_like(a)
+
+# FFT with cuFFT
+plan = cp.cuda.cufft.XtPlanNd(shape[1:],
+                              shape[1:], 1, shape[1]*shape[2], idtype,
+                              shape[1:], 1, shape[1]*shape[2], odtype,
+                              shape[0], edtype,
+                              order='C', last_axis=-1, last_size=None)
+
+plan.fft(a, out, cp.cuda.cufft.CUFFT_FORWARD)
+
+# out_temp = cp.asnumpy(out).astype(np.float32)
+# out_temp2 = out_temp.view(np.complex64)
+
+# FFT with NumPy
+a_np = cp.asnumpy(a).astype(np.float32)  # upcast
+a_np = a_np.view(np.complex64)
+out_np = np.fft.fftn(a_np, axes=(-2,-1))
+# out_np_temp = out_np.astype(np.complex64)
+out_np = np.ascontiguousarray(out_np).astype(np.complex64)  # downcast
+out_np = out_np.view(np.float32)
+out_np = out_np.astype(np.float16)
+
+# don't worry about accruacy for now, as we probably lost a lot during casting
+print('ok' if cp.mean(cp.abs(out - cp.asarray(out_np))) < 0.1 else 'not ok')
+
+gpu_out = out.get()
+diff = gpu_out[0][0] - out_np[0][0]
+
+mse = np.sum(np.power(diff,2))/len(diff)
+print(f'MSE: {mse}')
+
+# plt.figure()
+# plt.plot(10*np.log10(np.power(np.abs(out_np[0][0]),2)))
+# plt.show()
+
+# plt.figure()
+# plt.plot(10*np.log10(np.power(np.abs(diff),2)))
+# plt.show()
+
+
+# --- FP32 ---
+# num_iter = 200
+BATCH = np.int32(1)
+# vec_len = np.power(2,16)
+
+# d_first_real = (np.ones((1,), dtype=np.float32))
+# d_second_real = (np.zeros((vec_len-1,), dtype=np.float32))
+
+# data_real = np.concatenate([d_first_real, d_second_real])
+# data = data_real
+data = a.get()
+data = data[0][0]
+
+# speedup_log = []
+# speedup_log_with_transfer = []
+# cpu_time_log = []
+# gpu_time_log = []
+# gpu_time_transfer_log = []
+
+# Create a stream
+stream = cuda.Stream()
+
+a = cuda.aligned_empty((len(data),), dtype=np.float32, alignment=resource.getpagesize())
+c = cuda.aligned_empty((int(len(data)/2+1),), dtype=np.complex64, alignment=resource.getpagesize())
+
+a[:] = data
+
+# Allocate number of bytes required by A on the GPU
+a_gpu = cuda.mem_alloc(a.nbytes)
+c_gpu = cuda.mem_alloc(c.nbytes)
+
+# Pin the host memory
+a_pin = cuda.register_host_memory(a)
+c_pin = cuda.register_host_memory(c)
+
+# Make FFT plan
+plan = cf.cufftPlan1d(len(a), cf.CUFFT_R2C, BATCH)
+
+# --- Memcopy from host to device
+# Asynchronously copy pinned array data to the gpu array
+cuda.memcpy_htod_async(a_gpu, a_pin, stream)
+# Execute FFT plan
+res = cf.cufftExecR2C(plan, int(a_gpu), int(c_gpu))
+
+# Transfer gpu array data to pinned memory
+cuda.memcpy_dtoh_async(c_pin, c_gpu, stream)
+
+# Synchronize
+cuda.Context.synchronize()
+
+# diff = gpu_out[0][0] - c_pin
+# mse = np.sum(np.power(diff,2))/len(diff)
+# print(f'MSE: {mse}')
+
+plt.figure()
+plt.plot(10*np.log10(np.power(np.abs(c_pin),2)))
+plt.plot(10*np.log10(np.power(np.abs(gpu_out[0][0]),2)))
+plt.show()
