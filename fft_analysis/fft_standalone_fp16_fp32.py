2f775a889cda7852da3daa218b7e8cdaa2e63027
avanderbyl <avanderbyl@sarao.ac.za>
2022-05-18 10:23:16 +0000

    Separated out data generation for constant, wgn and cw.

--- fft_analysis/fft_standalone_fp16_fp32.py
+++ fft_analysis/fft_standalone_fp16_fp32.py
@@ -1,5 +1,6 @@
 # Pinned memory based on example: https://gist.github.com/sjperkins/d9e6db1b2d6038febb72
 # FFT (FP16) Example: https://docs.cupy.dev/en/stable/user_guide/fft.html
+from cmath import pi
 import pycuda.driver as cuda
 import resource
 import numpy as np
@@ -10,7 +11,7 @@ from pycuda import gpuarray
 import matplotlib.pyplot as plt
 import cupy as cp
 
-N = 4096
+N = 2**18
 shape = (1, 1, N)  # input array shape
 
 def _pack_real_to_complex(data_in):
@@ -21,8 +22,17 @@ def _pack_real_to_complex(data_in):
         r +=2
     return data_out
 
-def generate_data():
-    # --- Generate Data for tests ---
+def generate_data(src, scale):
+    if src == 'wgn':
+        return _generate_data_wgn(scale)
+    elif src == 'cw':
+        return _generate_data_cw(scale)
+    elif src == 'const':
+        return _generate_constant(scale)
+
+
+def _generate_data_wgn(scale):
+    # Option 1: Generate WGN
     # Store the input/output arrays as fp16 arrays twice as long, as complex32 is not yet available
     # Additonal note: CuPy is expecting the data as complex. To input real data create array as R+j0.
 
@@ -34,17 +44,46 @@ def generate_data():
 
     # Option 2: Real input with complex formatting. Generate N random samples and either use as 
     # a N-valued real array or create a complex-valued array with imag as zero (0j).
-    a_in = 0.005*cp.random.random((shape[0], shape[1], shape[2])).astype(cp.float16)
+    a_in = scale*cp.random.random((shape[0], shape[1], shape[2])).astype(cp.float16)
     # a_real = cp.zeros((shape[0], shape[1], 2*shape[2]), dtype=np.float16)
     a_real = _pack_real_to_complex(a_in)
 
+    return (a_in, a_real)
+
+def _generate_data_cw(scale):
+    # Option 2: Generate CW
+    # Store the input/output arrays as fp16 arrays twice as long, as complex32 is not yet available
+    # Additonal note: CuPy is expecting the data as complex. To input real data create array as R+j0.
+
+    # Real input with complex formatting. Generate N random samples and either use as 
+    # a N-valued real array or create a complex-valued array with imag as zero (0j).
+    # a_in = 0.005*cp.random.random((shape[0], shape[1], shape[2])).astype(cp.float16)
+    # lin = cp.linspace(0,1,shape[2])
+    # a_in = scale*cp.cos(2*pi*lin)
+
+    f = shape[2]/512
+    in_array = np.linspace(-(f*np.pi), f*np.pi, shape[2])
+    a_in = scale*np.cos(in_array)
+    a_in = a_in.reshape(shape)
+
+
+    # plt.figure(1)
+    # plt.plot(a_in[0][0])
+    # plt.show()
+
+    a_real = _pack_real_to_complex(a_in)
+
+    # return (0, 1)
+    return (a_in, a_real)
+
+def _generate_constant(scale):
     # Option 3: Static vector array 
-    # vec_len = shape[2]
-    # d_first_real = cp.ones((1,), dtype=np.float16)
-    # d_second_real = cp.zeros((vec_len-1,), dtype=np.float16)
-    # b = cp.concatenate([d_first_real, d_second_real])
-    # b = b.reshape(shape)
-    # a_in = b
+    vec_len = shape[2]
+    d_first_real = scale*cp.ones((1,), dtype=np.float16)
+    d_second_real = cp.zeros((vec_len-1,), dtype=np.float16)
+    a_in = cp.concatenate([d_first_real, d_second_real])
+    a_in = a_in.reshape(shape)
+    a_real = _pack_real_to_complex(a_in)
     return (a_in, a_real)
 
 def _fft_cpu(a_real):
@@ -147,9 +186,17 @@ def _fft_gpu_fp32(a_in):
     return c_pin[0:int(N/2)]
 
 def display_results(fft_cpu_out, fft_gpu_fp32_out, fft_gpu_fp16_out):
+    # plt.figure()
+    # plt.plot(10*np.log10(np.power(np.abs(fft_cpu_out),2)))
+    # plt.plot(10*np.log10(np.power(np.abs(fft_gpu_fp32_out),2)))
+    # plt.plot(10*np.log10(np.power(np.abs(fft_gpu_fp16_out),2)))
+    # plt.show()
+
     plt.figure()
     plt.plot(10*np.log10(np.power(np.abs(fft_cpu_out),2)))
+    plt.figure()
     plt.plot(10*np.log10(np.power(np.abs(fft_gpu_fp32_out),2)))
+    plt.figure()
     plt.plot(10*np.log10(np.power(np.abs(fft_gpu_fp16_out),2)))
     plt.show()
 
@@ -173,8 +220,9 @@ def analyse_results(fft_cpu_out, fft_gpu_fp32_out, fft_gpu_fp16_out):
 
 
 def main():
-    # Generate data
-    a_in, a_real = generate_data()
+    # Generate data: options, 'wgn', 'cw', 'const'
+    scale = 0.1
+    a_in, a_real = generate_data('cw', scale)
 
     # Run fp16 FFT
     fft_gpu_fp16_out = _fft_fp16_gpu(a_real)
